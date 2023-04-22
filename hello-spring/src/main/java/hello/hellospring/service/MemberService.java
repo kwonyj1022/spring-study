b68b8bf47361bb321171abad9b0348edcc2c09d0
kwonyj1022 <kwonyj1022@naver.com>
2023-04-23 00:46:04 +0900

    AOP가 필요한 상황 (AOP를 사용하지 않았을 때)

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -20,9 +20,16 @@ public class MemberService {    // 리포지토리와 도메인을 활용해서 
      * 회원 가입
      */
     public Long join(Member member) {
-        validateDuplicateMember(member);    // 중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
+        long start = System.currentTimeMillis();
+        try {
+            validateDuplicateMember(member);    // 중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs + "ms");
+        }
     }
 
     private void validateDuplicateMember(Member member) {
@@ -36,7 +43,14 @@ public class MemberService {    // 리포지토리와 도메인을 활용해서 
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers = " + timeMs + "ms");
+        }
     }
 
     public Optional<Member> findOne(Long memberId) {
